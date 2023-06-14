e666c290d526fd309533f3c6ba1514c1f1ecc1f2
Lalo Flores <eflores@crudcreativo.com>
2023-06-13 17:56:09 -0700

    First modules

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ProductsComponent } from './products/products.component';
+import { SharedComponent } from './shared/shared.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MessagesComponent,
+    ProductsComponent,
+    SharedComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
