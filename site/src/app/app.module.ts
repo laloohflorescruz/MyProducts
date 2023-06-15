b3f67769a06147d32cab9f3582fc01e5a2dfedd7
Lalo Flores <eflores@crudcreativo.com>
2023-06-15 08:56:27 -0700

    Bootstrap mockup

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -3,24 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ProductsComponent } from './products/products.component';
 import { SharedComponent } from './shared/shared.component';
 import { UserComponent } from './user/user.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './pageNotFound/pageNotFound.component';
 
 @NgModule({
-  declarations: [
+  declarations: [	
     AppComponent,
-    HomeComponent,
+    WelcomeComponent,
     MessagesComponent,
     ProductsComponent,
     SharedComponent,
-    UserComponent
-  ],
+    UserComponent,
+    PageNotFoundComponent
+   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    //HttpClientModule,
+    //InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
+    RouterModule.forRoot([
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', component: PageNotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
